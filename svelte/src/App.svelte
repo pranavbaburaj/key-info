07b3e761689d22be65f568075516a8160abb3d63
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-26 12:23:16 +0530

    Added some basic elements

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
-	export let name: string;
+	import Input from './components/Input.svelte'
+
+	let headerValue:string = "Enter the key"
+	let inputValue:string = ""
+
+	function keylistener(event:Event) {
+		console.log(event)
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p class="header">{headerValue}</p>
+	<Input placeholder="" keylistener={keylistener} value={inputValue}/>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	:root {
+	    background-color : #36393F;
+		/* --foreground : #7289DA; */
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.header {
+		color : #FF7B7A;
+		font-weight: 400;
+		letter-spacing: 1px;
+		text-transform: capitalize;
 	}
 </style>
---
+++ svelte/src/components/Input.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let placeholder:string;
+    export let keylistener:Function;
+    export let value:string;
+</script>
+
+<main>
+    <input type="text" placeholder={placeholder}  value={value} on:keydown={event => {
+        keylistener(event)
+    }}>
+</main>
+
+<style>
+    input {
+        width: 50%;
+        border: 2px solid #000000;
+        outline: 0;
+        padding: 10px;
+        border-radius: 10px;
+        margin: 15px 15px 15px 15px;
+        background: rgb(53, 59, 63);
+        border: 2px solid #000000;
+        color: #FF7B7A;
+        font-size: 20px;
+    }
+</style>
